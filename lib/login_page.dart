a176240d9a379d7452ae9d6ee3d200f9eeb5c554
naoufal-mft <naoufal.meftahi@insa.ueuromed.org>
2023-06-13 17:03:15 +0200

    feat(page de registration): ajout de qlqs champs

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,133 +6,144 @@ class CustomLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan.shade900,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Center(
-                  child: ShaderMask(
-                    blendMode: BlendMode.srcATop,
-                    shaderCallback: (Rect bounds) {
-                      return LinearGradient(
-                        colors: [
-                          Colors.white,
-                          Colors.white38.withOpacity(0.8),
-                          Colors.transparent,
-                        ],
-                        stops: [0.0, 0.4, 1.0],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ).createShader(bounds);
-                    },
-                    child: Image.asset(
-                      'assets/user.png',
-                      width: 400.0,
-                      height: 400.0,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.cyan.shade900,
+              Colors.cyan.shade500,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: ShaderMask(
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                          colors: [
+                            Colors.white,
+                            Colors.white.withOpacity(0.8),
+                            Colors.transparent,
+                          ],
+                          stops: [0.0, 0.8, 1.0],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ).createShader(bounds);
+                      },
+                      child: Image.asset(
+                        'assets/user.png',
+                        width: 400.0,
+                        height: 400.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Adresse mail',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 20.0),
+                Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Adresse mail',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Mot de passe',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 10.0),
+                Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Mot de passe',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
-                  ),
-                  obscureText: true,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Actions à effectuer lors de la connexion
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.cyan,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 15.0),
-                ),
-                child: Text(
-                  'Se connecter',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => RegistrationPage(),
-                    ),
-                  );
-                },
-                child: Text(
-                  'S\'inscrire',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                    decoration: TextDecoration.underline,
+                    obscureText: true,
                   ),
                 ),
-              ),
-              Spacer(),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: IconButton(
+                SizedBox(height: 20.0),
+                ElevatedButton(
                   onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('Aide'),
-                        content: Text('Voici une description de l\'application.'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text(
-                              'Fermer',
-                              style: TextStyle(color: Colors.cyan),
-                            ),
-                          ),
-                        ],
+                    // Actions à effectuer lors de la connexion
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.cyan,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                  ),
+                  child: Text(
+                    'Se connecter',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegistrationPage(),
                       ),
                     );
                   },
-                  icon: Icon(
-                    Icons.help,
-                    color: Colors.white,
-                    size: 30.0,
+                  child: Text(
+                    'S\'inscrire',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      decoration: TextDecoration.underline,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Spacer(),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Aide'),
+                          content: Text('Voici une description de l\'application.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                'Fermer',
+                                style: TextStyle(color: Colors.cyan),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.help,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
